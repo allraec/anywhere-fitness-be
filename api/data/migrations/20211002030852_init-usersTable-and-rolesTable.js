87cf690818aeba5719e26b5fd08c0b159001084e
Allison Castaneda <allraec.dev@gmail.com>
2021-10-03 01:42:59 -0500

    added roles and users table

--- api/data/migrations/20211002030852_init-usersTable-and-rolesTable.js
+++ api/data/migrations/20211002030852_init-usersTable-and-rolesTable.js
@@ -1,13 +1,19 @@
 
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', () => {
-        users.increments('user_id');
-        users.string('username').notNullable().unique();
-        users.
+    .createTable('roles', (roles) => {
+      roles.increments('role_id').notNullable();
+      roles.string('rolename').notNullable();
+    })
+    .createTable('users', (users) => {
+      users.increments('user_id');
+      users.string('username').notNullable().unique();
+      users.string('password').notNullable();
+      users.integer('role_id').references('role_id').inTable('roles').notNullable();
     })
 };
 
-exports.down = function(knex) {
-  
+exports.down = async (knex) => {
+  await knex.schema.dropTableIfExists('users');
+  await knex.schema.dropTableIfExists('roles');
 };
